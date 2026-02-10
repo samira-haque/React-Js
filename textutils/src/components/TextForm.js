fe430799da6b943e6f119f119a9e02e435982bf9
Samira Haque <sh6007530@gmail.com>
2026-02-10 21:06:08 +0600

    js

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -13,12 +13,19 @@ export default function TextForm(props) {
     const [text,setText] = useState("Enter Text Here");
     // setText("new Text"); to change the state
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
   <div class="mb-3">
     <textarea class="form-control"value={text} onChange={handleOnChange} id="MyTextBox" rows="8"></textarea>
   </div>
   <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
     </div>
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 }Minutes read</p>
+    </div>
+    </>
   )
 }
